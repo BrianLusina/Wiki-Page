6c5f8d9fce3ea6a98fc10ab39c0573b25fada885
BrianLusina <lusinabrian@gmail.com>
2016-12-05 20:39:13 +0300

    update webpack dev configurations

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react'
 import {render} from 'react-dom'
 import App from './components/App'
 import "./styles/styles.css"
+
 render(<App />, document.getElementById("app"), () => console.timeEnd('mini-wiki'));
