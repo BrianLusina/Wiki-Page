d7ad5cf37b178033cf89936b80347aa44180b22d
BrianLusina <lusinabrian@gmail.com>
2016-12-05 20:50:19 +0300

    import header component to APp

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+
+
+export default class App extends React.Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
+      <Header />
+    )
+  }
+}
