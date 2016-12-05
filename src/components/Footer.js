92a52dc208150380d2675eb220403bb4cbb289b3
BrianLusina <lusinabrian@gmail.com>
2016-12-05 21:23:48 +0300

    edit src files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ export default class Footer extends React.Component{
 
   render(){
     return(
-      <footer classNameName="primary-footer container group">
+      <footer className="primary-footer container group">
 
-          <small>Made with <i className="fa fa-heart"></i> and <i className="fa fa-music"></i> by <a href="http://codepen.io/mercury/">Mercury</a>
+          <small>Made with <i className="fa fa-heart"></i> and <i className="fa fa-music"></i> by <a href="http://codepen.io/thelusina/">The Lusina</a>
           </small>
 
           <nav className="nav">
@@ -17,7 +17,6 @@ export default class Footer extends React.Component{
                   <li><a href="https://www.linkedin.com/in/brianlusina" target="_blank"><i className="fa fa-linkedin fa-2x social"></i></a></li>
                   <li><a href="https://github.com/brianlusina" target="_blank"><i className="fa fa-github fa-2x social"></i></a></li>
                   <li><a href="https://twitter.com/brianlusina" target="_blank"><i className="fa fa-twitter fa-2x social"></i></a></li>
-                  <li><a href="" target="_blank"><i className="fa fa-reddit fa-2x social"></i></a></li>
                   <li><a href="https://medium.com/@brianlusina" target="_blank"><i className="fa fa-medium fa-2x social"></i></a></li>
                   <li><a href="https://plus.google.com/102302755150765254402" target="_blank"><i className="fa fa-google-plus fa-2x social"></i></a></li>
               </ul>
