78ba11bdba2f568c24b188f426c28652bcd4d02c
BrianLusina <lusinabrian@gmail.com>
2016-12-05 22:46:21 +0300

    key event handling in search bar

--- src/components/Section/Main.js
+++ src/components/Section/Main.js
@@ -4,13 +4,19 @@ import WikiResults from './WikiResults';
 export default class Main extends React.Component{
   constructor(){
     super();
+
+    this.state = {
+      inputText = ""
+    }
+
+    this._handleKeyPress = this._handleKeyPress.bind(this);
   }
 
   render(){
     return(
       <section>
           <div id="search-container">
-            <input type="text" placeholder="Wiki search..." id="input-text-container"/>
+            <input type="text" placeholder="Wiki search..." id="input-text-container"  onKeyUp={this._handleKeyPress}/>
             <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">
                 <i id="random-icon" className="fa fa-random fa-2x" hovertext="Click for a random article"></i>
             </a>
@@ -20,5 +26,11 @@ export default class Main extends React.Component{
           </div>
       </section>
     )
+  }
+
+  /**Handle the input text*/
+  _handleKeyPress(event){
+    event.preventDefault();
+    console.log(event.key);
   }
 }
